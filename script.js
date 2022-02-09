d8e29b5183d839a3fd86a8aea031dd1761f76830
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-02-09 13:42:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
+require([
+  "esri/Map", 
+  "esri/views/MapView",  
+  "esri/widgets/Legend",
+  "esri/layers/FeatureLayer"
+], (
+  Map, 
+  MapView, 
+  Legend,
+  FeatureLayer) => {
+   var map = new Map({
+       basemap: "topo-vector"
         });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
-        });
+    var view = new MapView({
+        map: map, 
+        container: "viewDiv",
+        center: [0, -1],
+        zoom: 3
       });
+          view.ui.add(
+          new Legend({
+            view: view
+          }),
+          "bottom-left"
+        );
+    
+  var featureLayer_1 = new FeatureLayer({
+    url: "https://services8.arcgis.com/zNrTBuYXV2f35M0U/arcgis/rest/services/Africa_Countries/FeatureServer"
+  });
+    map.add(featureLayer_1);
+
+ });
